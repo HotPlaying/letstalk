0429648706e064f94e110e40afbae62803a4a201
Hotplaying <tangrd98>
2020-09-14 23:31:55 +0800

    Exclude embed tomcat

--- src/main/java/com/main/letstalk/LetstalkApplication.java
+++ src/main/java/com/main/letstalk/LetstalkApplication.java
@@ -2,12 +2,21 @@ package com.main.letstalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LetstalkApplication {
+public class LetstalkApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(LetstalkApplication.class, args);
     }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+
 
 }
